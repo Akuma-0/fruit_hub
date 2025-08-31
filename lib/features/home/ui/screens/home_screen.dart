d7b478a6c2fb483967ef1f0d9a1d162011489a11
Hany <Hanym7md90@gmail.com>
2025-08-31 08:17:05 +0300

    feat: Add HomeScreenAppBar widget and integrate it into HomeScreenBody; update metadata and add avatar and notification icons

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fruit_hub/core/widgets/Custom_app_bar.dart';
 import 'package:fruit_hub/features/home/ui/screens/widgets/home_screen_body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,11 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(
-        title: '',
-        context: context,
-        isBackButtonVisible:false,
-      ),
+    
       body: HomeScreenBody(),
     );
   }
--- lib/features/home/ui/screens/widgets/home_screen_body.dart
+++ lib/features/home/ui/screens/widgets/home_screen_body.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/features/home/ui/screens/widgets/home_screen_app_bar.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: CustomScrollView(
+        slivers: [SliverToBoxAdapter(child: HomeScreenAppBar())],
+      ),
+    );
   }
 }
