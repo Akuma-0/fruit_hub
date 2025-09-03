7d6548fac72afcceca0cd168fa1ff8ec8a6d6b84
Hany <Hanym7md90@gmail.com>
2025-09-03 03:19:36 +0300

    feat: Add search text field with icons and integrate it into HomeScreenBody

--- lib/features/home/ui/screens/widgets/home_screen_body.dart
+++ lib/features/home/ui/screens/widgets/home_screen_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/features/home/ui/screens/widgets/home_screen_app_bar.dart';
+import 'package:fruit_hub/core/widgets/search_text_field.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
@@ -8,7 +9,15 @@ class HomeScreenBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: CustomScrollView(
-        slivers: [SliverToBoxAdapter(child: HomeScreenAppBar())],
+        slivers: [
+          SliverToBoxAdapter(child: HomeScreenAppBar()),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(children: [SizedBox(height: 16,),SearchTextField()]),
+            ),
+          ),
+        ],
       ),
     );
   }
